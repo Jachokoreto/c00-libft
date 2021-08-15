3cca0e2e2d32ea19141bc39d816a825e9d8fb205
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2021-08-15 21:37:18 +0800

    Update code

--- ft_split.c
+++ ft_split.c
@@ -60,3 +60,56 @@ char	*add_word(const char *s, int *i, char c)
 	idx1 = *i + 1;
 	return (ft_substr(s, idx1, idx2 - idx1));
 }
+
+// static int	wordcount(const char *s, char c)
+// {
+// 	int	i;
+// 	int	count;
+// 	int	isword;
+
+// 	i = 0;
+// 	count = 0;
+// 	isword = 0;
+// 	while (s[i])
+// 	{
+// 		if (isword == 0 && s[i] != c)
+// 		{
+// 			count++;
+// 			isword = 1;
+// 		}
+// 		else if (s[i] == c)
+// 			isword = 0;
+// 		i++;
+// 	}
+// 	return (count);
+// }
+
+// char	**ft_split(const char *s, char c)
+// {
+// 	int		i;
+// 	int		j;
+// 	int		k;
+// 	char	**ptr;
+
+// 	i = 0;
+// 	k = 0;
+// 	if (!s)
+// 		return (0);
+// 	ptr = ft_calloc((wordcount(s, c) + 1), 8);
+// 	if (!ptr)
+// 		return (0);
+// 	while (s[i])
+// 	{
+// 		if (s[i] != c)
+// 		{
+// 			j = 0;
+// 			while (s[i + j + 1] != c && s[i + j + 1])
+// 				j++;
+// 			i += j;
+// 			ptr[k] = malloc(j);
+// 			ft_strlcpy(ptr[k++], (char *)&(s[i - j]), j + 2);
+// 		}
+// 		i++;
+// 	}
+// 	return (ptr);
+// }
--- libft.h
+++ libft.h
@@ -15,6 +15,7 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, char *src, size_t n);
 size_t	ft_strlcat(char *dst, char *src, size_t n);
@@ -23,17 +24,19 @@ char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_atoi(const char *nptr);
+
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-void	*ft_calloc(size_t nmemb, size_t size);
-char	*ft_strdup(const char *s);
 
 //malloc
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
